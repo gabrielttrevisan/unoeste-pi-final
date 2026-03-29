c7885a34cf503d29d92eca72d538ad6b242da705
Gabriel Troian Trevisan <gabrieltr@outlook.com>
2026-03-29 13:27:43 -0300

    chore: initial commmit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 3004;
+const FRONTEND_URL = "http://localhost:3000";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: FRONTEND_URL }));
+
+app.use((_, res) => {
+  res.status(404).json({
+    data: null,
+    error: {
+      code: "NOT_FOUND",
+      message: "Resource Not Found",
+    },
+  });
+});
+
+app.use((err, _req, res, _next) => {
+  console.error(err);
+
+  res.status(500).json({
+    data: null,
+    error: {
+      code: "INTERNAL_SERVER_ERROR",
+      message: err.message ?? "Internal Server Error",
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🔥 Listening on PORT ${PORT}...`);
+});
